07c87e406e69dd9cd705b718e5962634a6c78848
Fred010 <fjbaafi18@gmail.com>
2024-10-05 13:40:22 +0000

    config files and return promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+/**
+ * Prototype function to return a Promise.
+ * @returns {Promise} A Promise.
+ */
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+// console.log()
+}
